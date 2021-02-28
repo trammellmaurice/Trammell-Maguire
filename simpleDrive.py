84ce1d09247d8145ca46ff33cd1feb84f65571b6
mars <zacharymtrammell@mail.com>
2021-02-28 13:46:09 -0500

    distance error and steerign error functions

--- simpleDrive.py
+++ simpleDrive.py
@@ -1,11 +1,35 @@
 import rospy
 import math
 from turtleAPI import robot
-
+from pid import pidController
+"""
+DISTANCE
+Find distance between two points
+"""
 def distance(start,fin):
     return math.sqrt(((fin[0]-start[0])**2)+((fin[1]-start[1])**2))
 
-turtle = robot()
+"""
+DISTANCE ERROR
+Find the error in distance from current position to desired position
+"""
+def d_error(desired):
+    return distance(turtle.getPositionTup(),desired)
+
+"""
+STEERING ERROR
+Find the error in yaw
+"""
+def s_error(desired):
+    # get current yaw
+    current = turtle.getPositionTup()[2]
+    #desired-current = error
+    return (desired-current)
+
+turtle = robot() # initialize robot
+steering = pidController(2) # make a p controller for steering with kp
+
+
 
 # initialize bot by getting world position
 start_pos = turtle.getPositionTup()
