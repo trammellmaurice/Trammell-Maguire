7ceced3dad66b40b66ecdbf9d4344d8b12609281
mars <zacharymtrammell@mail.com>
2021-02-28 13:22:40 -0500

    upate rospy not shutdown loop

--- simpleDrive.py
+++ simpleDrive.py
@@ -13,9 +13,9 @@ start_pos = turtle.getPositionTup()
 # make bot drive
 turtle.drive(0,5)
 
-#check position loop
-current_pos = turtle.getPositionTup()
-while (start_pos,current_pos) < 5:
+while not rospy.is_shutdown():
     current_pos = turtle.getPositionTup()
+    if distance(start_pos,current_pos) > 5:
+        break
 
 turtle.stop()
