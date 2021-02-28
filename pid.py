698797542cc73e068d3768f0a24760a3f34f996d
mars <zacharymtrammell@mail.com>
2021-02-28 16:48:05 -0500

    same

--- pid.py
+++ pid.py
@@ -25,7 +25,7 @@ class pidController:
         return (self.kd * (self.errors[len(self.errors)-1]-self.errors[len(self.errors)-2]))
 
     def pid(self):
-        return (self.p()+self.i())
+        return (self.p()+self.i()+self.d())
         #return (self.p())
 
 if __name__ == '__main__':
